9f78b4f23d5b4b5e2c1e2342f69a09a0fdfef191
kai <kai03210607@gmail.com>
2020-06-25 11:15:20 +0900

    エラー確認

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,11 @@ namespace v0625
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int a = 1;
+            MessageBox.Show(""+(10/a));
+        }
     }
 }
